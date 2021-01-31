b9e06907c9caac6be12059139139fcf72db0d46b
Vincent Mercier <vincent.mercier@qonto.eu>
2021-01-31 11:22:59 +0100

    chore(release.sh): Fix version

--- release.sh
+++ release.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# Generate new tag
+CURRENT_TAG=`git describe --abbrev=0 --tags`
+VERSION_BITS=(${CURRENT_TAG//./ }) #replace . with space so can split into an array
+NEW_TAG="${VERSION_BITS[0]}.${VERSION_BITS[1]}.$((${VERSION_BITS[2]}+1))"
+
 echo "Release of $(date)" > static/index.html
 git commit static/index.html -m 'chore(release): New release'
-git push
+git tag ${NEW_TAG}
+git push --tags
